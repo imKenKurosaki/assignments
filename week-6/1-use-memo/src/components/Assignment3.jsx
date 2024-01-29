044d6f109bad2e81b2e602579f6570cd114d777b
Kean Serna <keandelaserna11@gmail.com>
2024-01-29 09:29:47 +0800

    Adding my solution on week6 useMemo and useCallback

--- week-6/1-use-memo/src/components/Assignment3.jsx
+++ week-6/1-use-memo/src/components/Assignment3.jsx
@@ -8,11 +8,18 @@ export const Assignment3 = () => {
         { name: 'Chips', value: 20 },
         { name: 'Onion', value: 30 },
         { name: 'Tomato', value: 30 },
+        { name: 'Tomato', value: 30 },
+        { name: 'Tomato', value: 30 },
         // Add more items as needed
     ]);
 
     // Your code starts here
-    const totalValue = 0;
+    const totalValue = useMemo(() => {
+        let sum = 0;
+        items.map((item) => sum += item.value);
+
+        return sum;
+    }, []);
     // Your code ends here
     return (
         <div>
--- week-6/2-use-callback/src/components/Assignment2.jsx
+++ week-6/2-use-callback/src/components/Assignment2.jsx
@@ -1,32 +1,45 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, memo } from "react";
 
 // Create a component with a text input field and a button. The goal is to display an alert with the text entered when the button is clicked. Use useCallback to memoize the event handler function that triggers the alert, ensuring it's not recreated on every render.
-// Currently we only have inputText as a state variable and hence you might not see the benefits of 
+// Currently we only have inputText as a state variable and hence you might not see the benefits of
 // useCallback. We're also not passing it down to another component as a prop which is another reason for you to not see it's benefits immedietely.
 
+let timeoutId = 0;
 export function Assignment2() {
-    const [inputText, setInputText] = useState('');
+  const [inputText, setInputText] = useState("");
+  const [flag, setFlag] = useState(true);
 
-    // Your code starts here
-    function showAlert() {
+  // Your code starts here
+  const showAlert = useCallback(() => {
+    alert(inputText);
+  }, [flag]);
 
-    }
-    // Your code ends here
+  function set() {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      console.log("asdasd");
+      setFlag(!flag ? true : false);
+    }, 300);
+  }
+  // Your code ends here
 
-    return (
-        <div>
-            <input
-                type="text"
-                value={inputText}
-                onChange={(e) => setInputText(e.target.value)}
-                placeholder="Enter some text"
-            />
-            <Alert showAlert={showAlert} />
-        </div>
-    );
-};
-
-function Alert({showAlert}) {
-    return <button onClick={showAlert}>Show Alert</button>
+  return (
+    <div>
+      <input
+        type="text"
+        value={inputText}
+        onChange={(e) => {
+          setInputText(e.target.value);
+          set();
+        }}
+        placeholder="Enter some text"
+      />
+      <Alert showAlert={showAlert} />
+    </div>
+  );
 }
 
+const Alert = memo(function Alert({ showAlert }) {
+  console.log("ALERRRT IN FUNCITON");
+  return <button onClick={showAlert}>Show Alert</button>;
+});
